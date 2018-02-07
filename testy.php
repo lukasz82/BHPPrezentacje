bc03a31a50f746a6d0c73bc2a136267fd3f7a842
ŁukaszKa <lukasz.kazimierczak82@gmail.com>
2018-02-07 11:58:31 +0100

    wersja 1.0.7 - całkiem poprawne wyswietlanie podgladu test.php, dodanie zarządzania wątkami w celu panowania nad czasem i przestrzenią :D

--- testy.php
+++ testy.php
@@ -16,38 +16,27 @@
 <div class="container-fluid">
 <div class="row content">
    
-<div id="czas"></div>
+<div id="time"></div>
 <div id="Film"></div>
 <div id="test"></div>
-<?php 
-
-/*
-require('./Klasy/BazaDanych/DataBase.php');
-DataBase::InitializeDB();
-    $aktywne_zdarzenie = DataBase::GetDataFromDatabase("SELECT id_zdarzenia FROM aktywnezdarzenie WHERE id = 1");
-    $result = $aktywne_zdarzenie['result'];
-    $line = $result->fetch_assoc();
-    $id = stripslashes($line['id_zdarzenia']);
-    echo "Id = ".$id;
-    */
-?>
 
 <script>
 
+var count = 0;
+var counter = 0;
+let id = 0;
+//let play = false;
+let play = []; // Zakadam, że wszystkie elementy ustawione są na "false"
+let arr_init = false; // Zmienna bool sprawdzajaca cz tablica zostaa zainicjowana
 function getTime() 
 {
     return (new Date()).toLocaleTimeString();
 }
 
-var count = 0;
-var licznik = 0;
-var id = 0;
-var play_1 = false;
-var play_2 = false;
-
-$(document).ready(function() //czeka aż dokument zostanie wczytany
-{ 
-    $.ajax
+function getEventId()
+{
+    
+    return $.ajax
     ({
         type : 'get',
         url  : 'Funkcje/EventNow.php',
@@ -55,141 +44,108 @@ $(document).ready(function() //czeka aż dokument zostanie wczytany
         // Dodstaję callback z tabelą z bazy danych
         success:function(data)
         {
-            id = data;
             console.log(data);
-
-            $.ajax
-            ({
-                type : 'get',
-                url  : 'Funkcje/EventsFromDatabase.php',
-                // Do "data" przekazuję id zdarzenia, żeby je aktywować
-                data : {'id':id},
-                dataType : 'json',
-                // Dodstaję callback z tabelą z bazy danych
-                success:function(data)
-                {
-                    console.log(data);
-                    document.getElementById('Film').innerHTML =  "";
-                    //count = data.nazwa.length;
-
-                    setInterval(function() 
-                    {
-                        licznik++;
-
-                        if (licznik > 2)
-                        {
-                            document.getElementById('Film').innerHTML =  "";
-                            $.ajax
-                            ({
-                                type : 'get',
-                                url  : 'Funkcje/EventNow.php',
-                                // Do "data" przekazuję id zdarzenia, żeby je aktywować
-                                // Dodstaję callback z tabelą z bazy danych
-                                success:function(data)
-                                {
-                                    console.log("Id w kroku 2: " + id);
-                                    console.log("data w kroku 2: " + data);
-                                    if (id != data) 
-                                    {
-                                        id = data;
-                                        $.ajax
-                                        ({
-                                            type : 'get',
-                                            url  : 'Funkcje/EventsFromDatabase.php',
-                                            // Do "data" przekazuję id zdarzenia, żeby je aktywować
-                                            data : {'id':id},
-                                            dataType : 'json',
-                                            // Dodstaję callback z tabelą z bazy danych
-                                            success:function(data)
-                                            {
-                                                console.log(data);
-                                                document.getElementById('Film').innerHTML =  "";
-                                                count = data.nazwa.length;
-
-
-                                                var czas = getTime();
-                                                for (i=0;i<count;i++)
-                                                {
-
-                                                    // Usuwam z czasów znaki specjalne, zostawiam cyfry w celu porównanie przedziaów czasu jako wartości typu int
-                                                    var data_start = data.start[i].replace(/[^0-9\.]+/g, "");
-                                                    var data_stop = data.stop[i].replace(/[^0-9\.]+/g, ""); 
-                                                    var time_copy = czas.replace(/[^0-9\.]+/g, "");
-
-                                                    console.log("Play 2 data start: " + data_start);
-                                                    console.log(data_stop);
-                                                    console.log("Play 2 time copy: " + time_copy); 
-                                                    console.log("Play 2 status: " + play_2); 
-
-                                                    // Warunek odpalający filmik gdy czas jest równoważny dacie startu
-                                                    //if ( czas == data.start[i] )
-                                                    // Warunek odpalający filmik z danego przedzialu czasu
-                                                    if (data_start <= time_copy && data_stop > time_copy)
-                                                    {
-                                                        if (data_start <= time_copy && data_stop > time_copy)
-                                                        {
-                                                            if (play_2 == false)
-                                                            {
-                                                                document.getElementById('Film').innerHTML =  '<video width="800" height="600" controls autoplay loop><source src="Filmy/'+data.dir_filmu[i]+'" type="video/mp4">Your browser does not support the video tag.</video>';
-                                                                play_2 = true;
-                                                            }
-                                                        }   
-                                                        else 
-                                                        {
-                                                            play_2 = false;
-                                                        }
-                                                    }
-                                                }
-
-                                            }
-                                        });
-                                    }
-
-                                    var czas = getTime();
-                                    for (i=0;i<count;i++)
-                                    {
-
-                                        // Usuwam z czasów znaki specjalne, zostawiam cyfry w celu porównanie przedziaów czasu jako wartości typu int
-                                        var data_start = data.start[i].replace(/[^0-9\.]+/g, "");
-                                        var data_stop = data.stop[i].replace(/[^0-9\.]+/g, ""); 
-                                        var time_copy = czas.replace(/[^0-9\.]+/g, "");
-
-                                        console.log(data_start);
-                                        console.log(data_stop);
-                                        console.log(time_copy); 
-
-                                        // Warunek odpalający filmik gdy czas jest równoważny dacie startu
-                                        //if ( czas == data.start[i] )
-                                        // Warunek odpalający filmik z danego przedzialu czasu
-                                        if (data_start <= time_copy && data_stop > time_copy)
-                                        {
-                                            if (play_1 == false)
-                                            {
-                                                document.getElementById('Film').innerHTML =  '<video width="800" height="600" controls autoplay loop><source src="Filmy/'+data.dir_filmu[i]+'" type="video/mp4">Your browser does not support the video tag.</video>';
-                                                play_1 = true;
-                                            }
-                                        }   
-                                        else 
-                                        {
-                                            play_1 = false;
-                                        }
-                                    }
-
-                                    
-                                }
-                            });
-                            licznik = 0;
-                        }
-
-
-                        document.getElementById('czas').innerHTML =  czas;
-                    }, 5000);
-                }
-            });
-
-
         }
-    });        
+    })   
+}
+
+async function getDataFromDatabase() 
+{
+    console.log("id z eventa 1: " + id);
+    
+    if (id != await getEventId()) 
+    {
+        arr_init = false;
+    }
+
+    id = await getEventId();
+    console.log("id z eventa 2: " + id);
+
+    $.ajax
+    ({
+        type : 'get',
+        url  : 'Funkcje/EventsFromDatabase.php',
+        // Do "data" przekazuję id zdarzenia, żeby je aktywować
+        data : {'id':id},
+        dataType : 'json',
+        // Dodstaję callback z tabelą z bazy danych
+        success:function(data)
+        {
+            console.log(data);
+            //document.getElementById('Film').innerHTML =  "";
+            count = data.nazwa.length;
+
+            if (arr_init == false) 
+            {
+                play.splice(0, play.length);
+                document.getElementById('Film').innerHTML ="";
+                for (i=0; i<count; i++)
+                {
+                    play[play.length] = false;
+                }
+                arr_init = true;
+            }
+
+            //arr[arr.length] = 1;
+            //arr[arr.length] = 2;
+            arr_lenght = play.length;
+            console.log("arr[] lenght przed petla: " + arr_lenght); 
+
+            var time = getTime();
+            for (i=0;i<count;i++)
+            {
+                // Usuwam z timeów znaki specjalne, zostawiam cyfry w celu porównanie przedziaów timeu jako wartości typu int
+                var data_start = data.start[i].replace(/[^0-9\.]+/g, "");
+                var data_stop = data.stop[i].replace(/[^0-9\.]+/g, ""); 
+                var time_copy = time.replace(/[^0-9\.]+/g, "");
+
+
+                console.log("Play 2 data start: " + data_start);
+                console.log(data_stop);
+                console.log("Play 2 time copy: " + time_copy); 
+                console.log("Play 2 status: " + play[i]); 
+
+                // Warunek odpalający filmik z danego przedzialu timeu
+
+
+
+                if (play[i] == false)
+                {
+                    if (time_copy > data_start && time_copy < data_stop)
+                    {
+                       
+                        document.getElementById('Film').innerHTML =  '<video width="800" height="600" controls autoplay loop><source src="Filmy/'+data.dir_filmu[i]+'" type="video/mp4">Your browser does not support the video tag.</video>';
+                        //play = true;
+                        play[i] = true;
+                    } else { play[i] = false; }
+                } else if (play[i] == true)
+                {
+                    if ( (time_copy < data_start && time_copy < data_stop) || (time_copy < data_start && time_copy < data_stop) )
+                    {                        
+                        play[i] = false;
+                        console.log("jesli play == true dstart<t i dstop<t: " + play[i]); 
+                    }
+                } 
+            }
+        }
+    });
+}
+
+
+$(document).ready(function() //czeka aż dokument zostanie wczytany
+{ 
+    
+    setInterval(function() 
+    {
+        counter++;
+        if (counter > 5)
+        {
+            getDataFromDatabase();
+            counter = 0;
+        }
+        document.getElementById('time').innerHTML =  time;
+    }, 1000);
+
 });
 
 </script>
